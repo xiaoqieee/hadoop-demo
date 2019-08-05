b3daa4c277d362aacfd35c4a6bbe21c68ea9512e
xn025665 <fengqi1@xiaoniu66.com>
2019-08-05 16:02:32 +0800

    二次排序

---
+++ src/main/java/com/hawcore/hadoop/demo2/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * This package (org.dataalgorithms.chap01.mapreduce) contains source code  
+ * for chapter 01 of the Data Algorithms book published by O'Reilly.
+ * MapReduce/Hadoop solutions are provided here.
+
+ * @author Mahmoud Parsian
+ *
+ */
+package com.hawcore.hadoop.demo2;
+
+//rest of the file is empty
